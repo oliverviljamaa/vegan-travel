3fc9a8e5e1f3b049ef2687f0ae55e3e42064cfa0
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-08-11 20:06:19 +0000

    Updates

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+  export async function preload() {
+    const res = await this.fetch('translations.json');
+    const data = await res.json();
+
+    if (res.status === 200) {
+      return { translations: data };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+  export let translations;
+</script>
+
+<style>
+  p {
+    font-size: 1.75rem;
+    text-align: center;
+  }
+</style>
+
+<svelte:head>
+  <title>🌱 Vegan translations</title>
+</svelte:head>
+
+<p>{translations.GB}</p>
