b60f3a71c0cf1cbc94928295bd305dd61fa23880
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-08-11 22:59:11 +0300

    Remove most of bootstrapped app, add simple translations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,29 @@
+<script context="module">
+  export async function preload() {
+    const res = await this.fetch('translations.json');
+    const data = await res.json();
+
+    if (res.status === 200) {
+      return { translations: data };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+  export let translations;
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  p {
+    font-size: 1.75rem;
+    text-align: center;
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>🌱 Vegan translations</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>{translations.GB}</p>
